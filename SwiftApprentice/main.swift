198f1113ab86c003b7c9d11c20f4ada46d124e76
Nancy Lu <ylu@lcs.on.ca>
2019-11-22 14:33:11 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Lu, Yunjia on 2019-11-22.
+//  Copyright © 2019 Nancy Lu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
